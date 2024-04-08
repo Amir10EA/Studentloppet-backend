af45eec3d1ff083f42a1ccec89c95280934d5545
anfr990813 <anton.j.fredriksson@hotmail.com>
2024-04-08 14:12:20 +0200

    Jag skrev gurka

--- src/main/java/com/pvt152/StudentLoppet/MainController.java
+++ src/main/java/com/pvt152/StudentLoppet/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
     
     @GetMapping(value="/hello")
     public @ResponseBody String hello() {
-        return "det borde funka";
+        return "Gurka";
     }
     
 }
