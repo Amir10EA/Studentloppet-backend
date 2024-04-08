d85f1d77479d9c3ba9109b65e835b30b3eb7fe2a
BillboEliasJohansson <elias.swe.jh@gmail.com>
2024-04-08 13:21:25 +0200

    test

--- src/main/java/com/pvt152/StudentLoppet/MainController.java
+++ src/main/java/com/pvt152/StudentLoppet/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
     
     @GetMapping(value="/hello")
     public @ResponseBody String hello() {
-        return "hello";
+        return "det borde funka";
     }
     
 }
