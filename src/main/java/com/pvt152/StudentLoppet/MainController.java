25578025a4ddae8d89e23b34b383dac88e99b8df
Amir10EA <amirelabidou11@gmail.com>
2024-04-13 00:31:13 +0200

    Revert "Test för att lägga till en student i databasen"
    This reverts commit 52b5244448244c961f58a3a93b77a4878272c55c.

--- src/main/java/com/pvt152/StudentLoppet/MainController.java
+++ src/main/java/com/pvt152/StudentLoppet/MainController.java
@@ -1,37 +1,19 @@
 package com.pvt152.StudentLoppet;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
-@RequestMapping(path = "/pet")
+@RequestMapping(path="/pet")
 public class MainController {
-
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @GetMapping(value = "/hello")
+    
+    @GetMapping(value="/hello")
     public @ResponseBody String hello() {
         return "Nyare än nytt, Yeahoooo";
     }
-
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
-
-    @GetMapping(path = "/add/{studentName}/{University}")
-    public @ResponseBody String addNewStudent(@PathVariable String studentName, @PathVariable String university) {
-
-        Student student = new Student();
-        student.setName(studentName);
-        student.setUniversity(university);
-        studentRepository.save(student);
-        return "saved"; // return safe only if there was no errors, lämnar den såhär sålänge.
-    }
+    
 }
--- src/main/java/com/pvt152/StudentLoppet/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pvt152.StudentLoppet;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-
-}
