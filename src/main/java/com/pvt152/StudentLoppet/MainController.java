54ef486736f7c2f8a66b2c7900bacecad7ed5f22
Amir10EA <amirelabidou11@gmail.com>
2024-04-13 19:23:35 +0200

    Revert "commit för databashantering"
    This reverts commit d2988b9acf9002f6e5cf675c64616816f2638a10.

--- src/main/java/com/pvt152/StudentLoppet/MainController.java
+++ src/main/java/com/pvt152/StudentLoppet/MainController.java
@@ -1,71 +1,19 @@
 package com.pvt152.StudentLoppet;
 
-import java.time.LocalDateTime;
-import java.util.concurrent.ThreadLocalRandom;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import jakarta.persistence.criteria.CriteriaBuilder.In;
 
-import org.springframework.web.reactive.function.client.WebClient;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
-@RequestMapping(path = "/student")
-@CrossOrigin
+@RequestMapping(path="/pet")
 public class MainController {
-
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @GetMapping(value = "/hello")
+    
+    @GetMapping(value="/hello")
     public @ResponseBody String hello() {
-        return "Hello from the pet server";
+        return "Nyare än nytt, Yeahoooo";
     }
-
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Student> getStudents() {
-
-        for (Student checkStudent : studentRepository.findAll()) {
-
-            int strikes = 0;
-            if (checkStudent.getLastActiveDate() != null && (checkStudent.getLastActiveDate()
-                    .plusHours(checkStudent.getIntervalRegistrationDate()).isBefore(LocalDateTime.now()))) {
-                strikes++;
-            }
-
-            if (checkStudent.getRegistrationDate() != null && (checkStudent.getRegistrationDate()
-                    .plusHours(checkStudent.getIntervalRegistrationDate()).isBefore(LocalDateTime.now()))) {
-                strikes++;
-            }
-
-            if (strikes > 2) {
-                studentRepository.delete(checkStudent);
-            }
-        }
-
-        return studentRepository.findAll();
-    }
-
-    @GetMapping(path = "/add/{studentName}/{university}")
-    public @ResponseBody String addNewPet(@PathVariable String studentName, @PathVariable String university) {
-
-        Student student = new Student();
-        student.setName(studentName);
-        student.setUniversity(university);
-        studentRepository.save(student);
-        return "Saved";
-
-    }
-
+    
 }
