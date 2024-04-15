34fcc884d56131e47e6e6637d55548c6fe63728f
BillboEliasJohansson <elias.swe.jh@gmail.com>
2024-04-15 12:38:50 +0200

    test

--- src/main/java/com/pvt152/StudentLoppet/MainController.java
+++ src/main/java/com/pvt152/StudentLoppet/MainController.java
@@ -51,14 +51,14 @@ public class MainController {
 
     }
 
-    @GetMapping(path = "/add/{email}/{password}")
+    @GetMapping(path = "/add/test/{email}/{password}")
     public @ResponseBody String changePassword (@PathVariable String password, @PathVariable String email){
 
         User u = new User();
         
         u.setPassword(password);
         u.setEmail(email);
-        
+
         userRepository.save(u);
 
         return "Saved";
