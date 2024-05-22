dbcdd8b16da29e37ffae06607eb07023ff068a67
Yamen Barikhan <yamenbarikhan@gmail.com>
2024-05-22 20:53:47 +0200

    Junit för LeaderboardControllerTest och UniversityControllerTest

--- src/test/java/com/pvt152/StudentLoppet/controller/LeaderboardControllerTest.java
+++ src/test/java/com/pvt152/StudentLoppet/controller/LeaderboardControllerTest.java
@@ -12,7 +12,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import java.util.Arrays;
 import java.util.List;
 
